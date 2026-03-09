2981755b0f5816139c098bab02bdba7ef8926204
a25jiayeylin-tech <a25jiayeylin@inspedralbes.cat>
2026-03-09 14:23:32 +0100

    Include encabezado.php in listar.php

--- videojuego/web/a25jiayeylin.dam.inspedralbes.cat/public_html/videojuegos/listar.php
+++ videojuego/web/a25jiayeylin.dam.inspedralbes.cat/public_html/videojuegos/listar.php
@@ -1,5 +1,6 @@
 <?php
 $mysqli = include_once "conexion.php";
+include_once "encabezado.php";
 $resultado = $mysqli->query("SELECT id, nombre, descripcion FROM videojuegos");
 $videojuegos = $resultado->fetch_all(MYSQLI_ASSOC); ?>
 
